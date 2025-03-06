5f4396937b82d812f29314f424ea719f96563fd9
iasmyndircksen03 <148384818+iasmyndircksen03@users.noreply.github.com>
2025-03-06 13:41:40 -0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
-    
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
     botaoDeAcessibilidade.addEventListener('click', function (){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
        })
        
     let tamanhoAtualFonte = 1;
